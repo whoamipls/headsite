04c9b44b2d1860712161ab98b78133968f3d8cd0
whoamipls <739235242@qq.com>
2019-12-18 16:55:03 +0800

    实现资质图片懒加载

--- src/views/about/Qualification.vue
+++ src/views/about/Qualification.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
 		<Title :titles="['资质荣誉','Qualification honor','资质与认证','合规性是企业发展的基础，和德严格遵守国家法律法规的各项要求，积极参与行业标准的制定与推广']" />
+		<!--
 		<b-row v-for="i in 8">
 			<b-col>
 			<b-img thumbnail fluid :src="`images/about/honor${i*3-2}.png`" alt="Image 1"></b-img>
@@ -10,6 +11,18 @@
 			</b-col>
 			<b-col>
 			<b-img v-if="i*3<23" thumbnail fluid :src="`images/about/honor${i*3}.png`" alt="Image 3"></b-img>
+			</b-col>
+		</b-row>
+		-->
+		<b-row v-for="i in 8">
+			<b-col>
+				<b-img-lazy v-bind="mainProps" :src="getImageUrl(i*3-2)" alt="Image 1"></b-img-lazy>
+			</b-col>
+			<b-col>
+				<b-img-lazy v-if="i*3-1<23" v-bind="mainProps" :src="getImageUrl(i*3-1)" alt="Image 1"></b-img-lazy>
+			</b-col>
+			<b-col>
+				<b-img-lazy v-if="i*3<23" v-bind="mainProps" :src="getImageUrl(i*3)" alt="Image 1"></b-img-lazy>
 			</b-col>
 		</b-row>
 	</div>
@@ -22,9 +35,30 @@ export default {
 		Title
 	},
     mounted: function() {
-    }
+    },
+	methods: {
+		getImageUrl: function(id) {
+			return `images/about/honor${id}.png`;
+		}
+	},
+	data() {
+		return {
+			mainProps: {
+				center: true,
+				fluidGrow: true,
+				blank: true,
+				blankColor: '#bbb',
+				// width: 600,
+				// height: 400,
+				class: 'my-5'
+			}
+		}
+	}
 }
 </script>
 
 <style scoped lang="less">
+	.mt-5, .my-5 {
+		margin-top: 0rem!important;
+	}
 </style>
