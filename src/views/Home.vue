5006663b974ecbb4d8d796de896299900a3412de
whoamipls <739235242@qq.com>
2020-01-15 13:17:38 +0800

    压缩图片

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
 						<!-- SLIDE  -->
 						<li data-transition="random" data-slotamount="7" data-masterspeed="300" data-saveperformance="off">
 							<!-- MAIN IMAGE -->
-							<img src="assets/home/banner/1.jpg" alt="slider" data-bgposition="center center" data-bgfit="cover"
+							<img src="assets/home/banner/1.png" alt="slider" data-bgposition="center center" data-bgfit="cover"
 								data-bgrepeat="no-repeat">
 							<!-- LAYERS -->
 							<!-- LAYER NR. 2 -->
@@ -41,7 +41,7 @@
 						<!-- SLIDE  -->
 						<li data-transition="random" data-slotamount="7" data-masterspeed="300" data-saveperformance="off">
 							<!-- MAIN IMAGE -->
-							<img src="assets/home/banner/1.jpg" alt="slider" data-bgposition="center center" data-bgfit="cover"
+							<img src="assets/home/banner/1.png" alt="slider" data-bgposition="center center" data-bgfit="cover"
 								data-bgrepeat="no-repeat">
 							<!-- LAYERS -->
 							<!-- LAYER NR. 2 -->
@@ -83,19 +83,19 @@
 					<!-- OFFER -->
 					<div class="offers">
 						<ul class="row">
-							<li class="col-sm-4"> <img src="assets/home/mainwork/1.jpg" class="img-responsive" alt="">
+							<li class="col-sm-4"> <img src="assets/home/mainwork/1.png" class="img-responsive" alt="">
 								<h5>宇航中心</h5>
 								<hr class="main">
 								<p class="hdAnnotationL">北京和德宇航技术有限公司(以下简称和德宇)成立于2007年4月，是一家致力于微小卫星及星座技术应用于人类生活的技术创新服务型公司。公司总部位于北京市海淀区北理工国防科技产业园,在香港...</p>
 								<a href="javascript:void(0);" @click="Goto('/Mainwork/Special')">详细 <i class="fa fa-long-arrow-right"></i></a>
 							</li>
-							<li class="col-sm-4"> <img src="assets/home/mainwork/2.jpg" class="img-responsive" alt="">
+							<li class="col-sm-4"> <img src="assets/home/mainwork/2.png" class="img-responsive" alt="">
 								<h5>“天行者”规划建设</h5>
 								<hr class="main">
 								<p class="hdAnnotationL">“天行者“星座是由北京和德宇航技术有限公司提出的先进的微小卫星星座，提供窄带空间物联网双向数据通信（DCS）和全球船舶位置监控（AIS），全球航空器监控（ADS-B）等空间服务。”天行者”星座由48...</p>
 								<a href="javascript:void(0);" @click="Goto('/Mainwork/Skywalker')">详细 <i class="fa fa-long-arrow-right"></i></a>
 							</li>
-							<li class="col-sm-4"> <img src="assets/home/mainwork/3.jpg" class="img-responsive" alt="">
+							<li class="col-sm-4"> <img src="assets/home/mainwork/3.png" class="img-responsive" alt="">
 								<h5>地面站系统建设</h5>
 								<hr class="main">
 								<p class="hdAnnotationL">根据公司全球服务体系，和德宇航分别在中国、东南亚、中亚、南亚、中东、中亚、非洲、美洲等区域成立海外子公司，建设地面站，组建全球地面站网，以及提供本地化的卫星应用服务，满足“天行者”星座在轨运...</p>
@@ -129,42 +129,42 @@
 					<div class="items row col-4">
 						<!-- ITEM -->
 						<article class="portfolio-item pf-branding-design pf-web-design">
-							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/1.jpg"> </a> </div>
+							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/1.png"> </a> </div>
 							<div class="portfolio-overlay">
 								<div class="position-center-center"> <a href="#."><i class="ion-link"></i></a> </div>
 							</div>
 						</article>
 						<!-- ITEM -->
 						<article class="portfolio-item pf-photography">
-							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/2.jpg"> </a> </div>
+							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/2.png"> </a> </div>
 							<div class="portfolio-overlay">
 								<div class="position-center-center"> <a href="#."><i class="ion-link"></i></a> </div>
 							</div>
 						</article>
 						<!-- ITEM -->
 						<article class="portfolio-item pf-web-design pf-branding-design">
-							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/3.jpg"> </a> </div>
+							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/3.png"> </a> </div>
 							<div class="portfolio-overlay">
 								<div class="position-center-center"> <a href="#."><i class="ion-link"></i></a> </div>
 							</div>
 						</article>
 						<!-- ITEM -->
 						<article class="portfolio-item pf-branding-design pf-digital-art">
-							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/6.jpg"> </a> </div>
+							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/6.png"> </a> </div>
 							<div class="portfolio-overlay">
 								<div class="position-center-center"> <a href="#."><i class="ion-link"></i></a> </div>
 							</div>
 						</article>
 						<!-- ITEM -->
 						<article class="portfolio-item pf-web-design pf-digital-art pf-branding-design">
-							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/4.jpg"> </a> </div>
+							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/4.png"> </a> </div>
 							<div class="portfolio-overlay">
 								<div class="position-center-center"> <a href="#."><i class="ion-link"></i></a> </div>
 							</div>
 						</article>
 						<!-- ITEM -->
 						<article class="portfolio-item pf-branding-design pf-digital-art">
-							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/5.jpg"> </a> </div>
+							<div class="portfolio-image"> <a href="#."> <img alt="" src="assets/home/news/5.png"> </a> </div>
 							<div class="portfolio-overlay">
 								<div class="position-center-center"> <a href="#."><i class="ion-link"></i></a> </div>
 							</div>
