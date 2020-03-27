5e53a75c1eca79f610aac5bc94aafc59042d6b39
whoamipls <739235242@qq.com>
2020-03-27 22:13:00 +0800

    “组件出口”重新改回以前版本；

--- src/views/mainwork/spacecenter/ComponentExport.vue
+++ src/views/mainwork/spacecenter/ComponentExport.vue
@@ -1,64 +1,40 @@
 <template>
     <div>
-        <PageTitle :titles="['航天重大专项配套','Major aerospace special support','宇航产品出口']" />
-        <p class="hdPara">随着中国在航天领域综合实力和技术能力的大幅提升，中国宇航产品也获得世界客户的青睐。 “技术成熟、可靠性高、性价比好” 是中国宇航级产品的三大优势，得到欧洲主流宇航企业的关注及认可。和德宇航公司作为中国首批商业航天公司之一，13年来密切关注国际商业航天动态，为中国宇航产品出口发挥积极作用。已成功将国内的星敏、太敏、热控薄膜、太阳能电池片等产品出口至瑞士、意大利、南非等国家。</p>
-        <p class="hdPara hdPMT">With the substantial improvement of China's space comprehensive strength and technological capability, Chinese space products have an emerging presence in the international space market.</p>
-        <p class="hdPara hdPMT">"Mature technology, high reliability and good cost performance" are the three advantages of China's space products, which have drawn the attention and recognition of mainstream space enterprises. The products ranges from EPS, solar panels, OBC, TTC, ADCS, structures, composite materials, etc. Founded in 2007, we’ve built a strong partnership with Chinese manufacturers and serve as an authorized distribution partner and in many cases the exclusive distribution partner for the international markets. To look at the full picture our products portfolio, please <a href='assets/mainwork/spacecenter/componentexport/1.pdf' download='Chinese Space Products Catalog_HEAD.pdf'>download</a> for review. If you are interested in reaching us, please send an email to Ms. Chen at <a href='javascript:void(0)'>charlotte.chen@head-aerosapce.com</a>.</p>
-        <p class="hdPara hdPMTB">As one of the first commercial space companies in China, HEAD has been closely following international commercial space developments for 13 years, playing an active role in China's space products export. The company has successfully exported products such as star tracker, sun sensor, thermal control film and solar cells to Switzerland, Italy and South Africa.</p>
-        <p class="hdHead1 hdH1MTB">主要产品 Products</p>
-        <p class="hdHead1 hdH1MT hdIconRect">太阳能电池片 Solar Cells</p>
-        <p class="hdPara">三结砷化镓太阳能电池 Triple-Junction GaAs Solar Cell</p>
-        <p class="hdHead1 hdH1MT">特点 Features</p>
-        <p class="hdPara">高效、可靠、热稳定性好、抗辐射能力强。</p>
-        <p class="hdPara">high-efficiency, high reliability, thermal stability and strong radiation resistance.</p>
-        <p class="hdHead1 hdH1MT">应用 Applications</p>
-        <p class="hdPara">应用于遥感、气象、导航、科学实验卫星等领域。所承担的任务成功率为100%.</p>
-        <p class="hdPara">Applied on remote-sensing, weather, navigation and science experiment satellites, and so on. The success rate of mission undertaken is 100%.</p>
-        <p class="hdHead1 hdH1MT">主要参数-27% Typical parameters of solar cell - 27%</p>
+        <PageTitle :titles="['宇航中心','Space Center','组件出口']" />
+        <p class="hdPara">随着中国在航天领域实力和能力的大幅提升，中国的航天产品也得到世界的关注。“技术成熟、可靠性高、性价比好” 是中国宇航级产品的三大优势，并得到了欧洲等主流宇航企业的关注及认可。和德宇航公司作为中国首批商业航天公司之一，13年来密切关注国际商业航天动态，为中国产品走出去贡献了自己的力量。目前已成功将国内的星敏、太敏、热控薄膜、太阳能电池片等产品出口至瑞士、意大利、南非等国。<a href='assets/mainwork/spacecenter/componentexport/1.pdf' download='Chinese Space Products Catalog_HEAD.pdf'>点击此处获取完整产品清单</a></p>
+        <p class="hdHead1 hdH1MTB">主要产品</p>
+        <p class="hdHead1 hdH1MB hdIconRect">滚珠轴承飞轮FW-GR.260.5-A</p>
+        <p class="hdHead1 hdH1MTB">产品介绍</p>
+        <p class="hdPara">该产品是一种机电集成的产品，拥有数字控制接口。它采用高精度速度测量单元，提升了角动量控制的精度，具有出色的零速度特性。</p>
+        <p class="hdHead1 hdH1MTB">应用范围</p>
+        <p class="hdPara">中地球轨道/近地轨道卫星</p>
+        <p class="hdHead1 hdH1MTB">主要参数</p>
         <div class="imgContainer100 hdPMTB">
             <div class="row">
-                <div class="col-sm-12">
+                <div class="col-sm-6">
                     <img src="assets/mainwork/spacecenter/componentexport/1.png" />
                 </div>
-            </div>
-        </div>
-        <p class="hdPara">Applications: Applicable for LEO, MEO and GEO.</p>
-        <p class="hdHead1 hdH1MT">主要参数-28.6% Typical parameters of solar cell - 28.6%</p>
-        <div class="imgContainer100 hdPMTB">
-            <div class="row">
-                <div class="col-sm-12">
+                <div class="col-sm-6">
                     <img src="assets/mainwork/spacecenter/componentexport/2.png" />
                 </div>
             </div>
         </div>
-        <p class="hdPara">Applications: Planed for high-resolution satellite, applicable for LEO, MEO and GEO.</p>
-        <p class="hdHead1 hdH1MT hdIconRect">M2离子推进器 M2-ion thruster</p>
-        <p class="hdPara">For 20kg satellites, M2-type low-power microwave ion thruster has the advantages of low power consumption, light weight, high specific impulse, reliable operation and easy use. The maximum thrust is 0.5Mn; the specific impulse is better than 1000s; the total system weight is less than 3.2kg; the total impulse is more than 4400s; and the total system power consumption is less than 45W (when thrust is 0.5mN). At present, the M2 low-power microwave ion thruster is a leading high-performance ion propulsion system designed for satellites with power consumption less than 50W.</p>
-        <p class="hdHead1 hdH1MT">产品介绍 Product introduction</p>
-        <p class="hdPara">The effective beam diameter of M2 ion thruster is 2cm. It adopts microwave ionization principle and dual-gate ion optical system. It has the advantages of simple structure, reliable operation, convenient operation, and wide range of adjustment of working state.</p> 
-        <p class="hdPara hdPMT">The maximum thrust is 0.5Mn; the power is less than 45W; the specific impulse exceeds 1100 s; Rated thrust 0.3mN with power less than 35W; When the thrust requirement is less than 0.3mN, the total power consumption of the propulsion system is less than 30W. The thruster has high reliability, 6000h life test has been completed and its performance is stable. Its integrated performance index is in the first class among the small power ion thrusters at home and abroad.</p>
-        <p class="hdPara hdPMT">The thruster uses a double-surplus, cold backup heater-cathode neutralizer, with a heating power consumption of only 6W and a service life exceeding 5000h. Two heater-cathodes backup can ensure the reliability of the thruster.</p>
-        <p class="hdHead1 hdH1MT">技术参数Technical specifications</p>
-        <p class="hdText hdIconTri">Thrust: ≥0.5mN</p>
-        <p class="hdText hdIconTri">Specific impulse: ≥1100s</p>
-        <p class="hdText hdIconTri">Total impulse: ≥4000Ns</p>
-        <p class="hdText hdIconTri">Max power: ≤45W（0.5mN）</p>
-        <p class="hdText hdIconTri">Rated power: ≤35W（0.3mN）</p>
-        <p class="hdText hdIconTri">On-orbit life: ≥3 years</p>
-        <p class="hdText hdIconTri">Weight: ≤3.2kg（in which Xenon propellant weighs 0.4kg）</p>
-        <p class="hdText hdIconTri">Volume: 2.3U detachable; 1.3U compact</p>
-        <p class="hdText hdIconTri">Starting mode: Instant-Start</p>
-        <p class="hdText hdIconTri">Restart times: ≥5000 times</p>
-        <p class="hdText hdIconTri">Restart time interval: Unlimited</p>
-        <p class="hdText hdIconTri">Working time: 24h non-stop</p>
-        <p class="hdText hdIconTri">Control mode: Satellite computer analog control</p>
-        <p class="hdHead1 hdH1MT hdIconRect">电推进 Electric propulsion</p>
-        <p class="hdPara">We(HEAD’s close partner) have been developing electric propulsion since 1974 and have become the most famous corporation in electric propulsion field of China. The electric propulsion segment covers two major electric propulsion areas – Ion and Hall. The ion electric propulsion such as LIPS-200 and LIPS-300 and the Hall electric propulsion such as LHT-40, LHT-100 were used or are planned to fulfil different space missions.</p>
-        <p class="hdPara">Find more information about the electric propulsion products in the <a href='assets/mainwork/spacecenter/componentexport/1.pdf' download='Chinese Space Products Catalog_HEAD.pdf'>Chinese Space Products Catalog</a>.</p>
-        <p class="hdHead1 hdH1MT hdIconRect">联系方式Contact：</p>
-        <p class="hdPara">Charlotte Chen, Key Account Manager</p>
-        <p class="hdPara">Email: charlotte.chen@head-aerospace.com</p>
-        <p class="hdPara">Tel.: + 86 18813962918</p>
+        <p class="hdHead1 hdH1MTB hdIconRect">三型高精度CCD星敏感器</p>
+        <p class="hdHead1 hdH1MTB">产品介绍</p>
+        <p class="hdPara">采用集成结构，基于ST-CCD2-2,选取大面积CCD检测器并增加高速通讯接口，具有低重量、高精度、良好动态性能、高可靠性等特点，产品接口为RS422或者LVDS。</p>
+        <p class="hdHead1 hdH1MTB">应用范围</p>
+        <p class="hdPara">该产品适用于地球同步轨道/中地球轨道/近地轨道卫星或者其他飞行器。</p>
+        <p class="hdHead1 hdH1MTB">主要参数</p>
+        <div class="imgContainer100 hdPMTB">
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="assets/mainwork/spacecenter/componentexport/3.png" />
+                </div>
+                <div class="col-sm-6">
+                    <img src="assets/mainwork/spacecenter/componentexport/4.png" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
